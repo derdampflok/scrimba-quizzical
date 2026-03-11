f34a295e180ce8ad376f1579b3e49f0de61ad1f6
derdampflok <kroft.domi@t-online.de>
2026-03-11 17:10:53 +0100

    add quiz and cover page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useState } from 'react'
 import './App.css'
+import CoverPage from './components/CoverPage'
+import Quiz from './components/Quiz/Quiz'
+
 
 export default function App() {
-  return <h1>App</h1>
+  const [showCoverPage, setShowCoverPage] = useState(true)
+
+  function startQuiz() {
+    setShowCoverPage(false)
+  }
+
+  return (
+    <main>
+      {showCoverPage ? <CoverPage startQuiz={startQuiz} /> : <Quiz />}
+    </main>
+  )
 }
---
+++ src/components/CoverPage.jsx
@@ -0,0 +1,8 @@
+export default function CoverPage(props) {
+    return (
+        <>
+            <h1>Cover Page</h1>
+            <button onClick={props.startQuiz}>Start Quiz</button>
+        </>
+    )
+}
